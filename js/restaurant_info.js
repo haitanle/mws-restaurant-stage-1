2818b8e866a185d98e1428a8861790b3ddf44596
tan <haitanle88@gmail.com>
2019-03-04 18:11:30 -0500

    add

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoidmkzdGZseWhpZ2giLCJhIjoiY2pzdXlidmxnMDFkZTQ0bGg2ODVsZ283MCJ9.JzScQVrvVmt3Remw8ytLdA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
