1075aafa3688116fcf89d7128c9c1248be1f5a44
tan <haitanle88@gmail.com>
2019-03-07 11:24:34 -0500

    fix restaurants-list layout

--- js/main.js
+++ js/main.js
@@ -172,6 +172,7 @@ createRestaurantHTML = (restaurant) => {
   li.append(neighborhood);
 
   const address = document.createElement('p');
+  address.className = 'restaurant-address';
   address.innerHTML = restaurant.address;
   li.append(address);
 
